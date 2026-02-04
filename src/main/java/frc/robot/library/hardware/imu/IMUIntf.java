d7dc281068f1a04c8820ec99f5fba94f828bdebe
k <sammy.bikki@gmail.com>
2026-02-04 15:48:13 -0500

    start fixing library for 2026

---
+++ src/main/java/frc/robot/library/hardware/imu/IMUIntf.java
@@ -0,0 +1,28 @@
+package org.mort11.library.hardware.imu;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+
+public interface IMUIntf {
+
+    public void setCanivore(String canivore);
+    
+    public double getAngle();
+
+    public double getRate();
+
+    // public double getAccelerationX();
+
+    // public double getAccelerationY();
+
+    // public double getAccelerationZ();
+
+    // public double getAcceleration();
+
+    public void reset();
+
+    public Rotation2d getRotation2d();
+
+    public Rotation3d getRotation3d();
+
+}
