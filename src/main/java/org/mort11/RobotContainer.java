ba50d034be6f5dbe7af16c6391cad4aee96c4c9a
Zoya S <zoya.shakil@motsd.org>
2026-02-21 09:24:47 -0500

    shooter speed

--- src/main/java/org/mort11/RobotContainer.java
+++ src/main/java/org/mort11/RobotContainer.java
@@ -61,7 +61,7 @@ import static org.mort11.configs.constants.PhysicalConstants.*;
 
 public class RobotContainer {
     private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+    private double MaxAngularRate = RotationsPerSecond.of(1.25).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
@@ -188,7 +188,8 @@ public class RobotContainer {
             //Shooter
             manualController.y().whileTrue(new PercentShoot(0.25));
 
-            endeffectorController.leftTrigger(TRIGGER_THRESHOLD).whileTrue(new SetShooter(3000));
+            endeffectorController.leftTrigger(TRIGGER_THRESHOLD).whileTrue(new SetShooter(2500));
+            endeffectorController.x().whileTrue(new SetShooter(6500));
             // LookUpTable.getNeededHoodAngle(3);
             // endeffectorController.x().whileTrue(new SetSuperShooter(
             //     () -> LookUpTable.getNeededShooterRPM(3), 
